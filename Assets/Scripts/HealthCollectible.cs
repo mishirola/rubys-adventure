d1f7ccc03aac1f112fb00cafa66b246b05aa143e
mishirola <rontenious@gmail.com>
2023-11-12 15:17:28 -0500

    Hi

---
+++ Assets/Scripts/HealthCollectible.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    public AudioClip collectedClip;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller.health < controller.maxHealth)
+        {
+            controller.ChangeHealth(1);
+            Destroy(gameObject);
+
+            controller.PlaySound(collectedClip);
+        }
+    }
+
+}
